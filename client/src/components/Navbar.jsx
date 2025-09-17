15a71633dc87a72cc547555db4449094070b548a
mudabirkowsar <mudabirkhanday@gmail.com>
2025-09-17 16:48:50 +0530

    added some css to Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom"; 
 import "./Navbar.css";
 
 export default function Navbar() {
-    return (
-        <nav className="navbar">
-            {/* Logo */}
-            <div className="navbar-logo">HandyCrew</div>
+  const [scrolled, setScrolled] = useState(false);
 
-            {/* Links */}
-            <ul className="navbar-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/services">Services</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-            {/* Buttons */}
-            <div className="navbar-buttons">
-                <Link to="/login">
-                    <button className="btn login">Login</button>
-                </Link>
-                <Link to="/provider">
-                    <button className="btn provider">Become a Provider</button>
-                </Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
+      <div className="navbar-logo">HandyCrew</div>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+      <div className="navbar-buttons">
+        <Link to="/login">
+          <button className="btn login">Login</button>
+        </Link>
+        <Link to="/provider">
+          <button className="btn provider">Become a Provider</button>
+        </Link>
+      </div>
+    </nav>
+  );
 }
