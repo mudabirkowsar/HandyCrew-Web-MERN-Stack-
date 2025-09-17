59632df47ecc8e68ecf5e53b6cb4947e6c95decd
mudabirkowsar <mudabirkhanday@gmail.com>
2025-09-17 17:08:37 +0530

    initialized backend with node

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("DB connected "))
+.catch((err) => {
+    console.log("Error in connection", err);
+    process.exit(1);
+} )
+
+app.get("/", (req, res) => {
+    res.send("Running huhu")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Running on port 5000")
+})
